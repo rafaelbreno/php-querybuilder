345b60f2ac8b75ae36562404507f15bd123764c9
Rafael Breno <rafael_breno@outlook.com>
2020-08-19 00:18:47 -0300

    Written and commented second test to get a param['sql']

--- tests/KeryTest.php
+++ tests/KeryTest.php
@@ -2,12 +2,14 @@
 
 namespace Test;
 
+use Kery\Builder;
 use Kery\Kery;
+use Kery\Writer;
 use PHPUnit\Framework\TestCase;
 
 class KeryTest extends TestCase
 {
-    /* First test
+    /* 1st Test Passed!
      * Just to know if composer.json is well configured
      * */
 //    public function testKeryFirst()
@@ -15,11 +17,17 @@ class KeryTest extends TestCase
 //        $this->assertSame('hi', Kery::init("hi"));
 //    }
 
-    public function testReturnSimpleSelectAll()
-    {
-        $expected = "SELECT * FROM authors";
-
-        $actual = Kery::init("authors");
-        $this->assertSame($expected, $actual);
-    }
+    /* 2nd Test Passed!
+     * This test was design to return a simple select query
+     * Just to be sure that I wrote right the "Closure" method
+     * */
+//    public function testReturnSimpleSelectAll()
+//    {
+//        $expected = "SELECT * FROM authors";
+//
+//        $actual = Kery::init("authors", function (Builder $writer) {
+//            $writer->test();
+//        });
+//        $this->assertSame($expected, $actual);
+//    }
 }
