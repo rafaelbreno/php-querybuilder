6646c808ef7c1f2707c3c1032cbbc71afca0ff20
Rafael Breno <rafael_breno@outlook.com>
2020-08-19 23:47:28 -0300

    Created Query generator, will receive the params and return the query string

---
+++ src/QueryGenerator.php
@@ -0,0 +1,57 @@
+<?php
+
+
+namespace Kery;
+
+
+use Helpers\Arr;
+use Helpers\Str;
+
+class QueryGenerator
+{
+    private $query = "";
+    protected $params = [];
+    public function __construct($params)
+    {
+        $this->params = $params;
+    }
+
+    public function returnQuery()
+    {
+        $this->buildQuery();
+        return $this->query;
+    }
+
+    protected function buildQuery()
+    {
+        foreach ($this->params as $key => $value)
+        {
+
+            echo "{$key}<br/>";
+            $this->{$key}($value);
+        }
+    }
+
+    protected function method($method = "SELECT")
+    {
+        $this->concatQuery($method);
+    }
+
+    protected function columns($columns)
+    {
+        $this->concatQuery(
+            Arr::concatWithDelimiter($columns)
+        );
+    }
+
+    protected function table($table)
+    {
+        $this->concatQuery("from {$table}");
+    }
+
+    private function concatQuery($queryPiece)
+    {
+        $this->query = implode(' ', [$this->query, $queryPiece]);
+    }
+
+}
